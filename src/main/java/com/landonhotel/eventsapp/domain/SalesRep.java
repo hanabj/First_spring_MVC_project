ca96d3a65b00ac2777147c78ced74b975e203a2d
p_hbenjebara <hana.bjebara@gmail.com>
2019-02-07 13:14:59 +0100

    Initial commit

---
+++ src/main/java/com/landonhotel/eventsapp/domain/SalesRep.java
@@ -0,0 +1,8 @@
+package main.java.com.landonhotel.eventsapp.domain;
+
+public class SalesRep {
+	private String lastName;
+	private String firstName;
+	private String emailAddress;
+	private String phoneNumber;
+}
